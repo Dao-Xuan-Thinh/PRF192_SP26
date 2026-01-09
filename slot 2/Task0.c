2e7f0bc6643e6c74404b0ed69e77f72cf25d9d6f
KhongPhaiNhen <daoxuanthinh.6886@gmail.com>
2026-01-09 11:42:34 +0700

    move files to the correct folder slot1 -> slot2

---
+++ slot 2/Task0.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+
+int main(){
+	int b;
+	
+	int a = 10;
+	b = 15;
+	printf("%d\n%d", a, b);
+}
---
+++ slot 2/Task3.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+
+//int main(){
+//	int a = 5;
+//	float b = 10.216;
+//	
+//	printf("a=%d\nb=%.3f\n", a, b);
+//	printf("%d + %.3f = %.2f", a, b, a+b);
+//}
+
+
+
+int main(){
+	float a = 5, b = 10.216;
+	
+	printf("a=%.0f\nb=%.3f\n", a, b);
+	printf("%.0f + %.3f = %.2f", a, b, a+b);
+}
