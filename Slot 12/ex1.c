62b93d198370d5f82fab3c237ffdab88023b9182
Thịnh Đào Xuân <81064900+Dao-Xuan-Thinh@users.noreply.github.com>
2026-02-27 11:04:31 +0700

    from ipad

--- Slot 12/ex1.c
+++ Slot 12/ex1.c
@@ -5,10 +5,27 @@
 
 
 //Functions
-int Total(){
-    int n, total
-    for(int i = 1; i >= n; i++){
-        if(n%)
+int Sum(int n){
+    int total;
+    for(int i = 1; i >= n/2; i++){
+        if(n%i == 0){
+            total += i;
+        }
+    }
+    return total;
+}
+
+
+int main(){
+    int n;
+    printf("Nhap n >");
+    scanf("%d", &n);
+    if(n <= 0){
+        printf("n khong hop le");
     }
 
+    int result = Sum(n);
+    printf("%d", result);
+
+    return 0;
 }
